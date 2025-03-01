07f799a1ec72dc1b12c8c009b390992c0ad13e6a
Nitr0 <bidintha2006@gmail.com>
2025-03-01 20:27:34 +0530

    new

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -224,7 +224,6 @@ func _process(delta: float) -> void:
 	# State machine tick
 	state.tick()
 	move_and_slide()
-	print(velocity.x)
 
 # ------------------------
 # Idle State
@@ -264,13 +263,10 @@ func _slash_state() -> Variant:
 	if is_on_floor():
 		if velocity.x != 0:
 			animation.play("Run_Slashing")
-			print("RUNING")
 		else:
 			animation.play("Slash")
-			print("NORMAL SLASH")
 	else:
 		animation.play("Air_Slashing")
-		print("AIR SLASHING")
 	await animation.animation_finished
 	
 	return _idle_state
