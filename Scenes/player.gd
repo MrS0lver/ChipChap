3dff1ba8886d7ef6e0e444fe09ab8864b0dcacc3
Nitr0 <bidintha2006@gmail.com>
2025-05-08 04:04:51 +0530

    New Update!

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,15 +1,11 @@
 extends CharacterBody2D
 
 @onready var animation: AnimatedSprite2D = $AnimatedSprite2D
-
 ## Create global State value
 var state: StateDelegate
-
 var speed: float = 400
 var jump_force: float = -400  # Upward force when jumping
 var last_platform_velocity = Vector2.ZERO
-
-
 var gravity_vector: Vector2 = ProjectSettings.get_setting("physics/2d/default_gravity_vector")
 var gravity_magnitude: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 var spawn_pos: Vector2  # Fixed spelling from 'spown_pos' to 'spawn_pos'
@@ -31,12 +27,9 @@ func _ready() -> void:
 	# Set default state
 	state.set_default_state(_idle_state)
 	
-
 func _physics_process(delta: float) -> void:
 	print(Engine.get_frames_per_second())
 	# FPS is VISIBLE for NOW NOT IN NODE BUT IN CONSOLE 
-	
-	
 	# Apply gravity
 	if not is_on_floor():
 		velocity += (get_gravity() * delta)
@@ -45,7 +38,6 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("Slide"):
 		state.set_state(_attack_state)
 		#$Shape.position = Vector2(0,22)
-	
 	# Movement
 	var direction := Input.get_axis("walk_left", "walk_right")
 	if direction:
@@ -53,7 +45,6 @@ func _physics_process(delta: float) -> void:
 	elif is_on_floor():
 		velocity.x = move_toward(velocity.x, 0, speed)
 	
-
 	# Flip animation based on direction
 	if direction != 0:
 		animation.flip_h = direction < 0  # True if moving left, False if moving right
@@ -61,7 +52,6 @@ func _physics_process(delta: float) -> void:
 	# Slash attack input
 	if Input.is_action_pressed("Slash"):
 		state.set_state(_slash_state)
-	
 	# Jump input
 	if  Input.is_action_just_pressed("Jump") and is_on_floor():
 		state.set_state(_jump_start)
@@ -76,18 +66,15 @@ func _physics_process(delta: float) -> void:
 	# Reset position if falling out of bounds
 	if position.y >= 670:
 		position = spawn_pos
-
 	move_and_slide()
 	# State machine tick
 	state.tick()
 	last_platform_velocity = get_platform_velocity()
-
 # ------------------------
 # Idle State
 # ------------------------
 func _idle_enter_state() -> void:
 	animation.play("Idle")
-
 func _idle_state() -> Variant:
 	$Shape.rotation_degrees = 0 
 	$Shape.position = Vector2(0,-48)
@@ -97,7 +84,6 @@ func _idle_state() -> Variant:
 
 func _idle_exit_state() -> void:
 	pass
-
 # ------------------------
 # Walk State
 # ------------------------
@@ -120,8 +106,6 @@ func _attack_state() -> Variant:
 		$Shape.position = Vector2(-26,-26)
 	await animation.animation_finished
 	return _idle_state
-	
-
 # ------------------------
 # Slash Attack State
 # ------------------------
@@ -136,7 +120,6 @@ func _slash_state() -> Variant:
 	await animation.animation_finished
 	
 	return _idle_state
-
 # ------------------------
 # Jump State
 # ------------------------
@@ -146,18 +129,15 @@ func _jump_enter():
 	animation.play("Jump_Start")
 	await animation.animation_finished
 	animation.play("Jump_Loop")
-
 func _jump_start() -> Variant:
 	if velocity.y > 0:  # Player is now falling
 		return _fall_state  # Transition to fall state
 	return null  # Stay in jump state
-
 # ------------------------
 # Falling State
 # ------------------------
 func _fall_enter():
 	animation.play("Falling")
-
 func _fall_state() -> Variant:
 	if is_on_floor():  # Player landed
 		return _idle_state  # Transition back to idle
